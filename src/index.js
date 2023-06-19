bbf460e76aaf3d32598aeaff6b030ce25f0a5ec6
lubna farheen <lubna.farheen2010@gmail.com>
2023-06-19 12:00:21 +0200

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM  from 'react-dom';
-import './AppStyle.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./AppStyle.css";
 import { createTheme, ThemeProvider } from "@mui/material";
-import MemberProfile from './MemberProfile';
-import StaffProfile from './StaffProfile';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import App from "./App";
+import Calendar from "./components/Calendar";
+import StaffCalendar from "./components/StaffCalendar";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "calendar",
+    element: <Calendar />,
+  },
+  {
+    path: "staffcalendar",
+    element: <StaffCalendarCalendar />,
+  },
+]);
 
 // Define your theme
 const theme = createTheme();
 
-ReactDOM.render( 
-<ThemeProvider theme={theme}>
-    <MemberProfile />
-    
-  </ThemeProvider>, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
